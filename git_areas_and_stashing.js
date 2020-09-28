04257feda69b96f8bc42bc0d54a60b454fb85a4c
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-28 02:58:17 -0400

    stash

---
+++ git_areas_and_stashing.js
@@ -0,0 +1,7 @@
+/**
+    1. Working Area -> Working Directory.
+    2. Staging Area ->  interfere of git.
+    3. Repository -> Files in git.
+    4. git ls-files -s -> show all commited files.
+    5. git stash -> save uncommited work.
+*/
