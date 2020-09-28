385aa66d5f9bbc29122a129d684cf273127271ac
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-28 00:58:07 -0400

    table of contents

---
+++ table_content.js
@@ -0,0 +1,13 @@
+/**
+    1. Git Foundations.
+    2. Git Areas and Stashing.
+    3. References, Commits, Branches.
+    4. Merging and Rebasing.
+    5. History and Diffs.
+    6. Fixing Mistakes.
+    7. Rebase and Amend.
+    8. Forks & Remote Repos.
+    9. Danger Zone.
+    10. GitHub.
+    11. Advanced GitHub.
+*/
