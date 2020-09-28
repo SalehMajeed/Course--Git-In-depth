7fe62e33f3585da5527736e750135a033ab46f12
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-28 01:46:31 -0400

    introduction about git

---
+++ git_foundations.js
@@ -0,0 +1,9 @@
+/*
+    1. git -> 
+        it is the storage to store data in the form of key and value where value can be retrieve using that key. it stores the compressed data in a blob,with metadata in header.
+    2. git hash-object --stdin.
+        we can generate SHA1 of the data using pipe and openssl sha1 or hash-object --stdin.
+    3. git store data in .git directory.
+    4. tree .git -> to show of tree graph of data.
+    5. git cat-file -t or -p(print contents)-> return type our binaray data like commit etc.
+*/
