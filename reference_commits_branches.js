92c727ec12d6103edd392eee50330605a4da7223
SalehMajeed <saleh.majeed8@gmail.com>
2020-09-28 12:10:49 -0400

    branches

---
+++ reference_commits_branches.js
@@ -0,0 +1,13 @@
+/*
+    1. references -> 
+        3 types-
+            1. tag.
+            2. branches.
+            3. HEAD.
+
+    2. git checkout 'branch name' -> for edit and changes also for change branch.
+    
+    3. tag -> store additional information about commit.
+
+    4. git show -> used for information about tag.
+*/
